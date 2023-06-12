fb9173f3f3576746f0b87adc77029352a2954e08
Guenole de Cadoudal <guenoledc@yahoo.fr>
2023-06-12 19:47:18 +0200

    Init vue3 + vuetify3

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-main>
+      <AppHeader />
+      <Home />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+  import Home from '@/views/Home.vue'
+  import AppHeader from '@/components/AppHeader.vue'
+</script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <div>
+    <v-toolbar
+      dark
+      prominent
+      
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-img inline :width="100" 
+        src="@/assets/so|bond.svg"
+      ></v-img>
+      <v-toolbar-title> explorer</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <!-- <v-btn icon>
+        <v-icon>mdi-export</v-icon>
+      </v-btn> -->
+    </v-toolbar>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: "AppHeader",
+  };
+
+</script>
